c15749ad4c78d6bbcfb5f100b90282ba8966d791
dong <dongbui303@gmail.com>
2019-12-13 21:00:32 +0700

    full

--- js/main.js
+++ js/main.js
@@ -1,13 +1,22 @@
 const tabItems = document.querySelectorAll('.tab-item');
-const tabContenttabItems = document.querySelectorAll('.tab-content-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
 // Select tab content item
 function selectItem(e) {
     removeBorder();
+    removeShow();
     // Add border to current tab
     this.classList.add('tab-border');
+    // Grab content item from DOM
+    const tabContentItems = document.querySelector(`#${this.id}-content`);
+
+    // Add show class 
+    tabContentItems.classList.add('show');
 }
 function removeBorder() {
-    tabItems.forEach(item => item.classList.remove('tab-border '));
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'));
 }
 
 
