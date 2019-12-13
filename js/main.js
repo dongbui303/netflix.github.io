e3f318f16edefdcfe70d14a6e67794b7e96ea57b
dong <dongbui303@gmail.com>
2019-12-13 17:42:05 +0700

    full

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContenttabItems = document.querySelectorAll('.tab-content-item');
+// Select tab content item
+function selectItem(e) {
+    removeBorder();
+    // Add border to current tab
+    this.classList.add('tab-border');
+}
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border '));
+}
+
+
+// Listen for tab click 
+tabItems.forEach(item => item.addEventListener('click', selectItem)); 
